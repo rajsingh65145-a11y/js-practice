76a4a03fcf7040714d30dfa362a1d1c259bbf707
rajsingh65145-a11y <rajsingh65145@gmail.com>
2026-07-14 20:54:40 +0530

    Modified function.js

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-// function declaration //
+/*// function declaration //
 
 function greet(name){
    return "hello"+name;
@@ -31,6 +31,29 @@ function processUser(name,callback){
     callback();
 }
 
-processUser("Harsh",function(){
+processUser("Harsh",function(){  // process User ek function hai jo upar create hua tha yaha usme value pass Hui hai//
     console.log("Done!");
-});
+});*/
+
+//----------------------------------------------------------------------------------------------------//
+
+//Higher Order //
+
+function multiplier(factor){// multiplier me do parameter hai jo ki return karte hai multipication of factor or num//
+    return function(num){
+        return num*factor;
+    };
+}
+
+const double=multiplier(2); // double declare kiya hai aur usme multiplier ki factor aur num ki value daal di hai //
+console.log(double(5))
+
+
+//1st question //
+
+function  calculatediscount (price,percent){
+   let discountprice= price*percent/100;
+   let finalprice=price-discountprice;
+   return finalprice;
+}
+console.log( calculatediscount(1000,10));
