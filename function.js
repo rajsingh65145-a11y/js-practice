a872950c3dc4e960d01d7244db2d4358f0fc9784
rajsingh65145-a11y <rajsingh65145@gmail.com>
2026-07-15 20:29:38 +0530

    fic js

--- function.js
+++ function.js
@@ -38,7 +38,7 @@ processUser("Harsh",function(){  // process User ek function hai jo upar create 
 //----------------------------------------------------------------------------------------------------//
 
 //Higher Order //
-
+/*
 function multiplier(factor){// multiplier me do parameter hai jo ki return karte hai multipication of factor or num//
     return function(num){
         return num*factor;
@@ -57,3 +57,55 @@ function  calculatediscount (price,percent){
    return finalprice;
 }
 console.log( calculatediscount(1000,10));
+*/
+
+//----------------------------------------------------------------------------//
+
+/*
+ const calculatediscount =(price,percent)=>{
+   let discountprice= price*percent/100;
+   let finalprice=price-discountprice;
+   return finalprice;
+}
+console.log( calculatediscount(1000,10));
+*/
+/*
+function processexpance(amount,callback){
+    console.log("processing: "+amount)
+    callback()
+}
+
+processexpance(10000,function(){
+    console.log("done");
+})
+
+let number=[1,2,3,4,5];
+
+let sqr=number.map((num)=>{
+    return num*num;
+});
+
+console.log(sqr);*/
+/*
+function createvalidator(miniamount){
+    return function(amount){
+        if (amount>=miniamount){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+};
+
+const isvalidexpanse=createvalidator(100);
+console.log(isvalidexpanse(50));
+console.log(isvalidexpanse(150));*/
+
+
+let number=[1,2,3,4,5];
+
+let sqr=number.map(num=>num*num);
+
+console.log(sqr);
